f265ef284bae852ea1b5470c11ca6eccaf52fcc5
DavidSCx <david_tiopatinhas@hotmail.com>
2021-05-11 19:48:38 -0300

    Agenda primeiro commit

---
+++ agenda/src/main/java/br/edu/usj/ads/pw/agenda/contato.java
@@ -0,0 +1,29 @@
+package br.edu.usj.ads.pw.agenda;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String nome;
+    String tipo;
+    String telefone;
+    
+}
